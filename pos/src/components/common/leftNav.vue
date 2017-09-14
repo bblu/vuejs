6e91372efadfe6fb196b65efc944247a87d778bd
bblu <gmwblu@gmail.com>
2017-09-14 17:57:48 +0800

    webpack test

---
+++ pos/src/components/common/leftNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="left-nav">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'leftNav',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
