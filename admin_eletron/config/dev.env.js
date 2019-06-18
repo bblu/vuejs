4de15e7e64dc0e55e735ccdcdb77e6b39aca8c93
bblu <gmwblu@gmail.com>
2019-06-18 18:09:47 +0800

    'treeview'

--- admin_eletron/config/dev.env.js
+++ admin_eletron/config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  BASE_API: '"http://localhost:8080/api"'
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
 }
